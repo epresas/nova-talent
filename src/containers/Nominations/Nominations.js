9d263bfc8cb89aacb9272b84bbd625bf1f5ef8ac
Ed <edmundoj.presas@gmail.com>
2021-03-29 10:58:31 +0200

    feat(*): Add responsive to table and routing
    tyle(*): Adjust responsive in tablet
    
    style(): Fix responsiveness in ipad
    
    feat(): Change title in index.html

--- src/containers/Nominations/Nominations.js
+++ src/containers/Nominations/Nominations.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import axios from 'axios';
 
 import NominationsTable from './NominationsTable/NominationsTable';
+import Button from '../../components/UI/Button/Button';
 
 const Container = styled.div`
   height: 100%;
   padding: 60px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+
+  h2 {
+    color: var(--color-white);
+    letter-spacing: .7px;
+    margin-bottom: 30px;
+    text-transform: uppercase;
+  }
 `;
 
 const Nominations = (props) => {
   const apiUrl = 'http://localhost:3001';
   const [nominations, setNominations] = useState(null);
+  const history = useHistory();
   let nominationEl = null;
 
   useEffect(() => {
@@ -22,12 +36,18 @@ const Nominations = (props) => {
     getNominations();
   }, []);
 
+  const onRedirect = () => {
+    history.push('/nominate');
+  };
+
   if (nominations) {
     const approvedOnlyNominations = nominations.filter(nomination => nomination.status === 'APPROVED');
     nominationEl = <NominationsTable info={approvedOnlyNominations} />;
     return (
       <Container>
+        <h2>Nominations</h2>
         {nominationEl}
+        <Button label='Add candidate' clicked={onRedirect} />
       </Container>
     )
   }
--- src/containers/Nominations/NominationsTable/NominationsTable.js
+++ src/containers/Nominations/NominationsTable/NominationsTable.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const Table = styled.table`
   background: var(--color-white);
+  border-collapse: collapse;
+  font-size: 1.2rem;
   text-align: center;
 
   td, th {
@@ -13,6 +15,70 @@ const Table = styled.table`
     max-width: 250px;
     -webkit-line-clamp: 3;
     -webkit-box-orient: vertical;
+  }
+
+  tr:nth-of-type(even) {
+    background: var(--color-grey-light);
+  }
+
+  tbody tr {
+    border-top: 2px solid var(--color-grey-medium-3);
+  }
+
+/* Visualize table like data list on moile and small tablets */
+  @media only screen and (max-width: 760px) {
+  font-size: 1.1rem;
+
+	/* Force table to not be like tables anymore */
+	table, thead, tbody, th, td, tr { 
+		display: block; 
+	}
+	
+	/* Hide table headers (but not display: none;, for accessibility) */
+	thead tr { 
+		position: absolute;
+		top: -9999px;
+		left: -9999px;
+	}
+	
+	tr { border: 1px solid var(--color-grey-medium-3); }
+	
+	td { 
+		/* Behave  like a "row" */
+		border: none;
+		border-bottom: 1px solid var(--color-grey-medium-3); 
+		position: relative;
+		padding-left: 50%; 
+	}
+	
+	td:before { 
+		/* Now like a table header */
+		position: absolute;
+		/* Top/left values mimic padding */
+		top: 6px;
+		left: 6px;
+		width: 45%; 
+		padding-right: 10px; 
+		white-space: nowrap;
+	}
+	
+	/*
+	Label the data
+	*/
+	td:nth-of-type(1):before { content: "Email"; }
+	td:nth-of-type(2):before { content: "Description"; }
+	td:nth-of-type(3):before { content: "Score (I | T)"; }
+	td:nth-of-type(4):before { content: "Status"; }
+	td:nth-of-type(5):before { content: "Date"; }
+  }
+`;
+
+const TableContainer = styled.div`
+  margin-bottom: 40px;
+  
+  @media only screen and (max-width: 760px) {
+    overflow-x: hidden;
+    overflow-y: auto;
   }
 `;
 
@@ -40,21 +106,23 @@ const NominationsTable = ({ info }) => {
   };
   let tableInfo = null;
 
-  if (info) {
-    tableInfo = <Table>
-      <Thead>
-        <tr>
-          <th>Email</th>
-          <th>Description</th>
-          <th>Score</th>
-          <th>Status</th>
-          <th>Date</th>
-        </tr>
-      </Thead>
-        <tbody>
-          {renderTableData()}
-      </tbody>
-    </Table>;
+  if (info.length) {
+    tableInfo = <TableContainer>
+      <Table>
+        <Thead>
+          <tr>
+            <th>Email</th>
+            <th>Description</th>
+            <th>Score (I | T)</th>
+            <th>Status</th>
+            <th>Date</th>
+          </tr>
+        </Thead>
+          <tbody>
+            {renderTableData()}
+        </tbody>
+      </Table>
+    </TableContainer>;
   return tableInfo
   }
 
